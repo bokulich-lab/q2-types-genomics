023496298bb2586af608bab49fbcd7b6da533432
Michal Ziemski <mziemski@ethz.ch>
2021-04-06 16:57:45 +0200

    added tests

--- q2_types_genomics/per_sample_data/_transformer.py
+++ q2_types_genomics/per_sample_data/_transformer.py
@@ -14,12 +14,12 @@ from q2_types_genomics.per_sample_data._util import _mag_manifest_helper
 from q2_types_genomics.per_sample_data._format import (
     MultiMAGManifestFormat,
     MAGSequencesDirFmt,
-    MultiFileDirectoryFormat
+    MultiFASTADirectoryFormat
 )
 
 
 @plugin.register_transformer
-def _1(dirfmt: MultiFileDirectoryFormat) \
+def _1(dirfmt: MultiFASTADirectoryFormat) \
         -> MAGSequencesDirFmt:
     return _mag_manifest_helper(
         dirfmt, MAGSequencesDirFmt, MultiMAGManifestFormat, DNAFASTAFormat)
