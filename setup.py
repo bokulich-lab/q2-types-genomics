099dfb752767f47118fc7b0876a4fa573eaec055
Michal Ziemski <mziemski@ethz.ch>
2021-05-11 13:00:38 +0200

    fixup! ENH: support for multidirectory bowtie index

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
              'data/manifests/*',
              'data/bowtie/unorganized/*',
              'data/bowtie/valid/sample1/mag1/*',
-             'data/bowtie/valid/sample2/mag1/*',]
+             'data/bowtie/valid/sample2/mag1/*']
     },
     zip_safe=False,
 )
