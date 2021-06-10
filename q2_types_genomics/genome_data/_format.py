08ef9bab6add780540e47e81a8252d3b094163a4
Michal Ziemski <mziemski@ethz.ch>
2021-06-10 10:34:22 +0200

    change SingleFileDirFmt to proper DirectoryFmt with a FileCollection

--- q2_types_genomics/genome_data/_format.py
+++ q2_types_genomics/genome_data/_format.py
@@ -11,16 +11,23 @@ import qiime2.plugin.model as model
 
 from ..plugin_setup import plugin
 
-GenesDirectoryFormat = model.SingleFileDirectoryFormat(
-    'GenesDirectoryFormat',
-    r'genes[0-9]+\.(fa|fasta)$',
-    DNAFASTAFormat
-)
 
-ProteinsDirectoryFormat = model.SingleFileDirectoryFormat(
-    'ProteinsDirectoryFormat',
-    r'proteins[0-9]+\.(fa|faa|fasta)$',
-    ProteinFASTAFormat
-)
+class GenesDirectoryFormat(model.DirectoryFormat):
+    genes = model.FileCollection(r'genes[0-9]+\.(fa|fna|fasta)$',
+                                 format=DNAFASTAFormat)
+
+    @genes.set_path_maker
+    def genes_path_maker(self, genome_id):
+        return '%s_genes.fasta' % genome_id
+
+
+class ProteinsDirectoryFormat(model.DirectoryFormat):
+    proteins = model.FileCollection(r'proteins[0-9]+\.(fa|faa|fasta)$',
+                                    format=ProteinFASTAFormat)
+
+    @proteins.set_path_maker
+    def proteins_path_maker(self, genome_id):
+        return '%s_proteins.fasta' % genome_id
+
 
 plugin.register_formats(GenesDirectoryFormat, ProteinsDirectoryFormat)
--- q2_types_genomics/genome_data/_transformer.py
+++ q2_types_genomics/genome_data/_transformer.py
@@ -6,12 +6,10 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-import glob
 import os
 
 import pandas as pd
 import skbio
-from q2_types.feature_data._transformer import _fastaformats_to_series
 
 from q2_types_genomics.genome_data import (
     GenesDirectoryFormat, ProteinsDirectoryFormat
@@ -21,13 +19,13 @@ from q2_types_genomics.plugin_setup import plugin
 
 
 CONSTRUCTORS = {
-    "DNA": skbio.DNA,
-    "RNA": skbio.RNA,
-    "protein": skbio.Protein
+    'DNA': skbio.DNA,
+    'RNA': skbio.RNA,
+    'protein': skbio.Protein
 }
 
 
-def _series_to_fasta(series, ff, seq_type="DNA"):
+def _series_to_fasta(series, ff, seq_type='DNA'):
     fp = str(f"{ff.path}/{series.name}.fasta")
     with open(fp, 'w') as fh:
         for id_, seq in series.iteritems():
@@ -36,39 +34,36 @@ def _series_to_fasta(series, ff, seq_type="DNA"):
                 skbio.io.write(sequence, format='fasta', into=fh)
 
 
-@plugin.register_transformer
-def _1(dirfmt: GenesDirectoryFormat) -> pd.DataFrame:
-    data = {}
-    for fp in sorted(glob.glob(f"{str(dirfmt)}/*.fa*")):
-        fname = os.path.splitext(os.path.basename(fp))[0]
-        data[fname] = _fastaformats_to_series(fp, constructor=skbio.DNA)
+def _multi_sequences_to_df(seq_iter_view):
+    data = {
+        os.path.splitext(fp)[0]: pds
+        for fp, pds in seq_iter_view
+    }
     df = pd.DataFrame.from_dict(data, orient='index')
     df.index.name = 'Genome ID'
     df = df.astype(str).replace({'nan': None})
     return df
+
+
+@plugin.register_transformer
+def _1(dirfmt: GenesDirectoryFormat) -> pd.DataFrame:
+    return _multi_sequences_to_df(dirfmt.genes.iter_views(pd.Series))
 
 
 @plugin.register_transformer
 def _2(df: pd.DataFrame) -> GenesDirectoryFormat:
     result = GenesDirectoryFormat()
-    df.apply(_series_to_fasta, axis=1, ff=result, seq_type="DNA")
+    df.apply(_series_to_fasta, axis=1, ff=result, seq_type='DNA')
     return result
 
 
 @plugin.register_transformer
 def _3(dirfmt: ProteinsDirectoryFormat) -> pd.DataFrame:
-    data = {}
-    for fp in sorted(glob.glob(f"{str(dirfmt)}/*.fa*")):
-        fname = os.path.splitext(os.path.basename(fp))[0]
-        data[fname] = _fastaformats_to_series(fp, constructor=skbio.Protein)
-    df = pd.DataFrame.from_dict(data, orient='index')
-    df.index.name = 'Genome ID'
-    df = df.astype(str).replace({'nan': None})
-    return df
+    return _multi_sequences_to_df(dirfmt.proteins.iter_views(pd.Series))
 
 
 @plugin.register_transformer
 def _4(df: pd.DataFrame) -> ProteinsDirectoryFormat:
     result = ProteinsDirectoryFormat()
-    df.apply(_series_to_fasta, axis=1, ff=result, seq_type="protein")
+    df.apply(_series_to_fasta, axis=1, ff=result, seq_type='protein')
     return result
