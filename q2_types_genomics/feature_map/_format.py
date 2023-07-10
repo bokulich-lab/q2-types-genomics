d3c4ae337459d7ddb49cd7525d5e13bced1e1a07
Michal Ziemski <mziemski@ethz.ch>
2023-07-10 10:10:40 +0200

    ENH: add FeatureMap[MAGtoContigs] types and formats

---
+++ q2_types_genomics/feature_map/_format.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+import json
+from uuid import UUID
+
+from qiime2.core.exceptions import ValidationError
+from qiime2.plugin import model
+
+
+class MAGtoContigsFormat(model.TextFileFormat):
+    def _validate_(self, level):
+        with self.path.open("r") as fh:
+            data = json.load(fh)
+            mag_ids = list(data.keys())
+
+            level_map = {"min": 1, "max": len(data)}
+            max_lines = level_map[level]
+
+            # assert keys are proper UUIDs and dict values are lists
+            for _id, contigs in list(data.items())[:max_lines]:
+                try:
+                    UUID(_id, version=4)
+                except ValueError:
+                    raise ValidationError(
+                        "MAG IDs must be valid UUID version 4 sequences. "
+                        f'Found "{_id}", which is invalid.'
+                    )
+
+                if not isinstance(contigs, list):
+                    raise ValidationError(
+                        "Values corresponding to MAG IDs must be lists of "
+                        f'contigs. Found "{contigs}" for MAG "{_id}".'
+                    )
+
+
+MAGtoContigsDirFmt = model.SingleFileDirectoryFormat(
+    "MAGtoContigsDirFmt", "mag-to-contigs.json", MAGtoContigsFormat
+)
